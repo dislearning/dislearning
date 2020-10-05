426873c2e1951b728c8236e0876332cee7d08ebf
Javier López Úbeda <jlopezcur@gmail.com>
2020-10-05 23:56:34 +0200

    feat: subdivide markdown renderer stories

--- src/components/renderers/MarkdownRenderer/MarkdownRender.stories.js
+++ src/components/renderers/MarkdownRenderer/MarkdownRender.stories.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import MarkdownRender from './MarkdownRender';
-import md from '!!raw-loader!./mocks/demo.md';
+
+import defaultMd from './__stories__/default.md';
+import codeMd from './__stories__/code.md';
+import mathMd from './__stories__/math.md';
 
 export default {
-  title: 'Renderers',
+  title: 'Renderers / Markdown',
 };
 
-export const markdownRender = () =>
-  <MarkdownRender source={md} />;
+export const Default = () => <MarkdownRender source={defaultMd} />;
+export const Code = () => <MarkdownRender source={codeMd} />;
+export const Math = () => <MarkdownRender source={mathMd} />;
 
-markdownRender.story = {
-  name: 'MarkdownRender',
-};
